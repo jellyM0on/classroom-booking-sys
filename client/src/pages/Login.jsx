79a1634b3ae4f57b17ddbda855c74a9072916897
jellyM0on <ylana.ong@gmail.com>
2025-05-30 15:43:46 +0800

    refactor initial login to presentation pattern

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,34 +3,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../configs/firebase";
 
-// TODO: Replace placeholder login page
-
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setError("");
-
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-      console.log(user);
-      sessionStorage.setItem("token", user.accessToken);
-      navigate("/");
-    } catch (err) {
-      console.error("Error signing in:", err);
-      setError(err.message);
-    }
-  };
-
+function Login({
+  email,
+  password,
+  error,
+  setEmail,
+  setPassword,
+  handleSubmit,
+}) {
   return (
     <main>
       <h1>Login</h1>
@@ -57,5 +37,44 @@ export default function Login() {
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
     </main>
+  );
+}
+
+export default function LoginContainer() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      console.log(user);
+      sessionStorage.setItem("token", user.accessToken);
+      navigate("/");
+    } catch (err) {
+      console.error("Error signing in:", err);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <Login
+      email={email}
+      password={password}
+      error={error}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      handleSubmit={handleSubmit}
+    />
   );
 }
