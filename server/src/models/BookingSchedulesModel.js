675984d1a99a2af61d3915eee55962f4baa48a05
jellyM0on <ylana.ong@gmail.com>
2025-06-16 17:32:54 +0800

    add day_of_week to bookingSchedules model

--- server/src/models/BookingSchedulesModel.js
+++ server/src/models/BookingSchedulesModel.js
@@ -40,6 +40,26 @@ const BookingSchedule = sequelize.define(
         },
       },
     },
+    day_of_week: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Day of week is required",
+        },
+        isInt: {
+          msg: "Day of week must be an integer",
+        },
+        min: {
+          args: [0],
+          msg: "Day of week must be between 0 and 5",
+        },
+        max: {
+          args: [5],
+          msg: "Day of week must be between 0 and 5",
+        },
+      },
+    },
     frequency: {
       type: DataTypes.ENUM("once", "daily", "weekly", "biweekly", "monthly"),
       allowNull: false,
