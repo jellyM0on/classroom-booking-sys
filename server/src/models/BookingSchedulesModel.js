148ea0a6b812e2568b6de423f5eea9169f978c35
jellyM0on <ylana.ong@gmail.com>
2025-06-16 17:24:48 +0800

    add bookings and booking schedules models

---
+++ server/src/models/BookingSchedulesModel.js
@@ -0,0 +1,76 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../configs/sequelize.js";
+
+const BookingSchedule = sequelize.define(
+  "BookingSchedule",
+  {
+    start_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Start date is required",
+        },
+      },
+    },
+    end_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "End date is required",
+        },
+      },
+    },
+    start_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Start time is required",
+        },
+      },
+    },
+    end_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Start time is required",
+        },
+      },
+    },
+    frequency: {
+      type: DataTypes.ENUM("once", "daily", "weekly", "biweekly", "monthly"),
+      allowNull: false,
+      defaultValue: "once",
+      validate: {
+        notNull: {
+          msg: "Frequency is required",
+        },
+        isIn: {
+          args: [["once", "daily", "weekly", "biweekly", "monthly"]],
+          msg: "Invalid frequency value",
+        },
+      },
+    },
+    status: {
+      type: DataTypes.ENUM("active", "inactive", "cancelled"),
+      allowNull: false,
+      defaultValue: "active",
+      notNull: {
+        msg: "Status is required",
+      },
+      isIn: {
+        args: [["active", "inactive", "cancelled"]],
+        msg: "Invalid status value",
+      },
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "booking_schedules",
+  }
+);
+
+export default BookingSchedule;
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,28 +1,36 @@
+import BookingSchedule from "./BookingSchedulesModel.js";
+import Booking from "./BookingsModel.js";
 import Building from "./BuildingsModel.js";
 import Department from "./DepartmentModel.js";
 import Room from "./RoomsModel.js";
 import User from "./UserModel.js";
 
+// A Department has many users. A user belongs to a department
 Department.hasMany(User, {
-  foreignKey: "departmentId",
-  as: "users",
+  foreignKey: {
+    name: "departmentId",
+    allowNull: false,
+  },
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
 
 User.belongsTo(Department, {
-  foreignKey: "departmentId",
+  foreignKey: {
+    name: "departmentId",
+    allowNull: false,
+  },
   as: "department",
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
 
+// A building has many rooms. A room belong to a building
 Building.hasMany(Room, {
   foreignKey: {
     name: "buildingId",
     allowNull: false,
   },
-  as: "rooms",
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
@@ -37,4 +45,87 @@ Room.belongsTo(Building, {
   onUpdate: "CASCADE",
 });
 
-export { Building, Department, Room, User };
+// A booking has booking schedules. A booking schedule belongs to a booking
+Booking.hasMany(BookingSchedule, {
+  foreignKey: {
+    name: "booking_id",
+    allowNull: false,
+  },
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+BookingSchedule.belongsTo(Booking, {
+  foreignKey: {
+    name: "booking_id",
+    allowNull: false,
+  },
+  as: "booking",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// A booking schedule belongs to a room
+Room.hasMany(BookingSchedule, {
+  foreignKey: {
+    name: "room_id",
+    allowNull: false,
+  },
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+BookingSchedule.belongsTo(Room, {
+  foreignKey: {
+    name: "room_id",
+    allowNull: false,
+  },
+  as: "room",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// A booking references user through submitted_by, facilitated_by, reviewed_by
+User.hasMany(Booking, {
+  foreignKey: {
+    name: "submitted_by",
+    allowNull: false,
+  },
+  as: "submittedBookings",
+});
+
+Booking.belongsTo(User, {
+  foreignKey: {
+    name: "submitted_by",
+    allowNull: false,
+  },
+  as: "submittedBy",
+});
+
+User.hasMany(Booking, {
+  foreignKey: {
+    name: "facilitated_by",
+    allowNull: false,
+  },
+  as: "facilitatedBookings",
+});
+
+Booking.belongsTo(User, {
+  foreignKey: {
+    name: "facilitated_by",
+    allowNull: false,
+  },
+  as: "facilitatedBy",
+});
+
+User.hasMany(Booking, {
+  foreignKey: "reviewed_by",
+  as: "reviewedBookings",
+});
+
+Booking.belongsTo(User, {
+  foreignKey: "reviewed_by",
+  as: "reviewedBy",
+});
+
+export { Booking, BookingSchedule, Building, Department, Room, User };
